2dbbb984ddc6cdf0291b7303719e13c588d51613
i1687438992 <1687438992@qq.com>
2024-06-05 17:24:16 +0800

    :bug: 文件上传

--- src/main/java/cn/xzhang/boot/controller/FileController.java
+++ src/main/java/cn/xzhang/boot/controller/FileController.java
@@ -11,13 +11,13 @@ import cn.xzhang.boot.model.enums.FileUploadBizEnum;
 import cn.xzhang.boot.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.RandomStringUtils;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
