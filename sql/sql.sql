eb3049dd334c62bfa24bcc9f0110177a77d7ec2f
小张 <1687438992@qq.com>
2024-09-06 22:16:20 +0800

    refactor(sql): 简化用户表并移除题库相关表
    - 用户表中的vip相关字段和分享码已被移除，以简化结构。
    - 移除了数据库中关于题库和题目表的定义，以减少不必要的数据库维护。

--- sql/sql.sql
+++ sql/sql.sql
@@ -16,12 +16,7 @@ create table if not exists user
     userName      varchar(256)  null comment '用户昵称',
     userAvatar    varchar(1024) null comment '用户头像',
     userProfile   varchar(512)  null comment '用户简介',
-    vipExpireTime datetime      null comment '会员过期时间',
-    vipCode       varchar(128)  null comment '会员兑换码',
-    vipNumber     bigint        null comment '会员编号',
-    shareCode     varchar(20)            DEFAULT NULL COMMENT '分享码',
-    inviteUser    bigint                 DEFAULT NULL COMMENT '邀请用户 id',
-    userRole      varchar(256)           default 'user' not null comment '用户角色：user/admin/vip',
+    userRole      varchar(256)           default 'user' not null comment '用户角色：user/admin',
     userStatus    tinyint                default 0 not null comment '用户状态（0正常 1停用）',
     creator       varchar(64)   NULL     DEFAULT '' COMMENT '创建者',
     createTime    datetime      NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
@@ -51,40 +46,5 @@ create table if not exists question_bank
     deleted       bit(1)        NOT NULL DEFAULT b'0' COMMENT '是否删除',
     index idx_title (title)
 ) comment '题库' collate = utf8mb4_unicode_ci;
-
--- 题目表
-create table if not exists question
-(
-    id         bigint auto_increment comment 'id' primary key,
-    title      varchar(256)  null comment '标题',
-    content    text          null comment '内容',
-    tags       varchar(1024) null comment '标签列表（json 数组）',
-    answer     text          null comment '推荐答案',
-    source     varchar(512)  null comment '题目来源',
-    needVip    tinyint                default 0 not null comment '仅会员可见（1 表示仅会员可见）',
-    viewNum    int                    default 0 not null comment '浏览量',
-    thumbNum   int                    default 0 not null comment '点赞数',
-    favourNum  int                    default 0 not null comment '收藏数',
-    creator    varchar(64)   NULL     DEFAULT '' COMMENT '创建者',
-    createTime datetime      NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-    updater    varchar(64)   NULL     DEFAULT '' COMMENT '更新者',
-    updateTime datetime      NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
-    deleted    bit(1)        NOT NULL DEFAULT b'0' COMMENT '是否删除',
-    index idx_title (title),
-    index idx_userId (creator)
-) comment '题目' collate = utf8mb4_unicode_ci;
-
--- 题库题目表（硬删除）
-create table if not exists question_bank_question
-(
-    id             bigint auto_increment comment 'id' primary key,
-    questionBankId bigint                             not null comment '题库 id',
-    questionId     bigint                             not null comment '题目 id',
-    questionOrder  int      default 0                 not null comment '题目顺序（题号）',
-    userId         bigint                             not null comment '创建用户 id',
-    createTime     datetime default CURRENT_TIMESTAMP not null comment '创建时间',
-    updateTime     datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
-    UNIQUE (questionBankId, questionId)
-) comment '题库题目' collate = utf8mb4_unicode_ci;
 
 
